93a5da817bce9f8fa4efeb770aa2654281b7c606
RajagopalMFAI <rajagopal.mfai@gmail.com>
2024-10-23 22:53:49 +0530

    completed

---
+++ public/script.js
@@ -0,0 +1,60 @@
+const socket = io('/');
+const videoGrid = document.getElementById('video-grid');
+const myPeer = new Peer(undefined, {
+  host: 'https://peer-testing.onrender.com', // Replace with your Peer server domain
+  path: '/myapp', // Path you used in the PeerJS server
+  secure: true,  // Enable this if using HTTPS
+  port: 443      // Use 443 for HTTPS, 80 for HTTP, or the port used by your Peer server
+});
+
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+const peers = {};
+
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {
+  addVideoStream(myVideo, stream);
+
+  myPeer.on('call', call => {
+    call.answer(stream);
+    const video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream);
+    });
+  });
+
+  socket.on('user-connected', userId => {
+    connectToNewUser(userId, stream);
+  });
+});
+
+socket.on('user-disconnected', userId => {
+  if (peers[userId]) peers[userId].close();
+});
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id);
+});
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement('video');
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream);
+  });
+  call.on('close', () => {
+    video.remove();
+  });
+
+  peers[userId] = call;
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener('loadedmetadata', () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
